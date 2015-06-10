c765aac81661ff36e851e5426cea845b10216a1c
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-06-10 19:37:31 +0530

    Working MVVM

--- Parallax/Parallax/MainPage.xaml.cs
+++ Parallax/Parallax/MainPage.xaml.cs
@@ -1,48 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using System.Diagnostics;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+using Parallax.MVVM;
 
 namespace Parallax
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
+            InitializeComponent();
+            //TODO: Make navigation cache persistant
+            NavigationCacheMode = NavigationCacheMode.Required;
+            Loaded += MainPage_Loaded;
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        async void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            EventViewModel eventViewModel = await EventViewModel.CreateTaskAsync();
+            stopwatch.Stop();
+            await new MessageDialog(stopwatch.ElapsedMilliseconds.ToString()).ShowAsync();
         }
     }
 }
--- Parallax/Parallax/Properties/AssemblyInfo.cs
+++ Parallax/Parallax/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Parallax")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Karthik Nishanth")]
 [assembly: AssemblyProduct("Parallax")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
